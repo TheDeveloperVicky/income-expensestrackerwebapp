a1a8d6b673a47295b8a882e21fe4f24926087463
Vignesh kumar <133491681+TheDeveloperVicky@users.noreply.github.com>
2023-05-15 22:46:07 +0530

    Update apiURL.js

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,3 +1,3 @@
-export const API_URL_USER = "http://localhost:8080/api/v1/users";
+export const API_URL_USER = "income-expensestrackerwebapp.vercel.app/api/v1/users";
 export const API_URL_ACC = "http://localhost:8080/api/v1/accounts";
 export const API_URL_TRANSACTION = "http://localhost:8080/api/v1/transactions";
